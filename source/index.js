fcb442634baf29d71a70512c4ead599b5823def4
anna-nicholls <anna.nicholls08@gmail.com>
2022-02-21 19:27:37 +1100

    Updated HTML, CSS and JS files
    Updated HTML, CSS and JS files from latest version (v22) in CodeSandbox

--- source/index.js
+++ source/index.js
@@ -3,13 +3,19 @@ let now = new Date();
 let todayDate = document.querySelector("#today-date");
 
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 let date = now.getDate();
 
 let days = [
   "Sunday",
   "Monday",
-  "Tueusday",
+  "Tuesday",
   "Wednesday",
   "Thurday",
   "Friday",
@@ -35,14 +41,80 @@ let month = months[now.getMonth()];
 
 todayDate.innerHTML = `${day} ${date} ${month}, ${hours}:${minutes}`;
 
-function displayCity(event) {
-  event.preventDefault();
-  let input = document.querySelector("#form-input");
-  console.log(input.value);
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#main-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
 
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = input.value;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#min").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#max").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
 }
 
-let city = document.querySelector("#search-form");
-city.addEventListener("submit", displayCity);
+function searchCity(city) {
+  let apiKey = "ecdfc99638105d33f56972e50bcc57cc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#form-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "ecdfc99638105d33f56972e50bcc57cc";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-btn");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Tokyo");
+
+// Code from previous activity below
+// F conversion and display in main
+
+function displayFarenheit(event) {
+  event.preventDefault();
+  let mainTemp = document.querySelector("#main-temperature");
+  let temperature = mainTemp.innerHTML;
+  mainTemp.innerHTML = `${Math.round((temperature * 9) / 5 + 32)}&degF`;
+}
+
+let farenheitLink = document.querySelector("#btnradio2");
+farenheitLink.addEventListener("click", displayFarenheit);
+
+// C conversion and display in main
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let mainTemp = document.querySelector("#main-temperature");
+  mainTemp.innerHTML = "26&degC";
+}
+
+let mainTemp = document.querySelector("#main-temperature");
+let temperature = mainTemp.innerHTML;
+
+let celsiusLink = document.querySelector("#btnradio1");
+celsiusLink.addEventListener("click", displayCelsius);
