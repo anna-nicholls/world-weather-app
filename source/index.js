4fe8b81ed830444a0367e78ac2a05aff12e7fa8e
anna-nicholls <anna.nicholls08@gmail.com>
2022-03-06 16:15:03 +1100

    Deleted forecast HTML and created display forecast function.
    Deleted forecast HTML and added it to function in JS.
    Created display forecast function.

--- source/index.js
+++ source/index.js
@@ -36,7 +36,38 @@ let celsiusTemperature = null;
 let mainTemp = document.querySelector("#main-temperature");
 let temperature = mainTemp.innerHTML;
 let celsiusLink = document.querySelector("#celsius-button");
-celsiusLink.addEventListener("click", displayCelsius);
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    
+          <div class="col-2">
+            
+              <div class="weather-forecast-date"><strong>${day}</strong></div>
+              <img
+                class="weekly-icon"
+                src="http://openweathermap.org/img/wn/50d@2x.png"
+                alt=""
+                width="80px"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperatures-max">30 </span> /
+              <span class="weather-forecast-temperatures-min">  24 </span>
+
+
+              </div>
+            </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -103,3 +134,4 @@ function displayCelsius(event) {
 }
 
 searchCity("Sydney");
+displayForecast();
